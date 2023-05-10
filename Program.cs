eecf60fdef4e2a34c3fb05b221892894064f76f4
perezlathaniel <perezlathaniel@outlook.com>
2023-05-09 23:30:54 -0400

    I learned how to declare & initialize an array, and how to manipulate it using loops.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@
             //Declares an array of integers named "numbers". Allocates memory on the heap for 5 elements in the array.
             int[] numbers = new int[5];
 
-            //Initializes each index of the array to an integer value.
+            //Initializes each element of the array to an integer value.
             numbers[0] = 4;
             numbers[1] = 8;
             numbers[2] = 15;
@@ -17,7 +17,9 @@
 
             //This is a method of indexing the array and then displaying the element at our specified index to the console application.
             Console.WriteLine(numbers[1]);
-            //This is a method used to determine how many elements are contained within an array.
+            
+            //The Length method is used to determine how many elements are contained within an object. In this case, we're determining
+            how many elements are in the "numbers" array.
             Console.WriteLine(numbers.Length);
             */
 
@@ -26,17 +28,34 @@
             //int[] numbers = new int[] { 1, 2, 3, 4, 5 };
 
             //This declares an array of strings called "names" and initializes it to contain the 3 string elements specified. 
-            string[] names = new string[] { "Dalia", "Wayne", "Lathaniel" };
+            //string[] names = new string[] { "Dalia", "Wayne", "Lathaniel" };
 
             //This is an example of how to display elements of an array of strings.
             //Console.WriteLine(names[0] + names[1] + names[2]);
 
-            //Real-world example of how arrays are manipulated using iteration loops. This example is a simple For iteration loop
-            for (int counter = 0; counter < names.Length; counter++)
+            /*Practical example of how arrays are manipulated using iteration loops. This example uses a simple For iteration loop.
+              for (int counter = 0; counter < names.Length; counter++)
+              {
+                 Console.WriteLine(names[counter]);
+              }
+            */
+
+            /*This is a simpler method of iteratively manipulating an array using the foreach loop.
+            foreach (string name in names)
             {
-                Console.WriteLine(names[counter]);
+                Console.WriteLine(name);
             }
-            
+            */
+
+            string phrase = "Good afternoon Mr. Lathaniel.";
+            char[] reverseOrder = phrase.ToCharArray();
+            Array.Reverse(reverseOrder);
+
+            foreach (char zigChar in reverseOrder)
+            {
+                Console.Write(zigChar);
+            }
+
 
             Console.ReadKey();
         }
